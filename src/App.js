313a2a4d0f8cb6d39dc61ccfaaa3b8876fe7bb7b
Luiz Alberto <Luiz.albuquerque99@gmail.com>
2024-07-24 16:02:35 -0300

    feat: implementing flag component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 import params from "./params";
 import Field from "./components/Field";
 
 export default props => {
 
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Iniciando o Mines!</Text>
             <Text>Tamanho da grande:
                 {params.getRowsAmount()} X {params.getColumnsAmount()}
@@ -19,7 +19,17 @@ export default props => {
             <Field opened nearMines={6}/>
             <Field mined opened/>
             <Field mined opened exploded/>
+            <Field flagged/>
+            <Field flagged opened/>
         </View>
     )
 }
 
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent: 'center',
+        alignItems: "center",
+        flex: 1
+    }
+})
